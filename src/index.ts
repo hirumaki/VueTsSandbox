df41875ffa138d6e0b12124160736277e2fa2ef5
hirumaki <index3583@gmail.com>
2021-05-10 17:46:47 +0900

    edit vue

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+import Vue from "vue";
+
+
+const msg:string = 'HelloWorld';
+console.log(msg);
+console.log('HEllo!!');
+
+const app = new Vue({
+    el: '#app-1',
+    data: {
+      message: 'Hello Vue!'
+    }
+})
